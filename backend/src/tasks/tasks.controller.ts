a81a6bfb0c409958a0172130b83395a52727a5f7
tsandovalr <tsandovalromay@gmail.com>
2022-01-08 02:14:16 -0400

    tasks controllers-1

--- backend/src/tasks/tasks.controller.ts
+++ backend/src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
 import { TasksService } from './tasks.service';
 import { Task } from 'src/entities/task.entity';
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Param, Patch, Post, Delete } from "@nestjs/common";
 
 @Controller()
 export class TasksController{
@@ -11,4 +11,23 @@ export class TasksController{
         return this.tasksService.getTasks();
     }
 
+    @Post() 
+    create(@Body() task:Task) {
+        return this.tasksService.addTask(task);
+    }
+
+    @Get(':id')
+    getOneTask(@Param('id') id: string): Promise<Task> {
+        return this.tasksService.findOne(Number(id));
+    }
+
+    @Patch('id')
+    updateTask(@Param('id') id: string, @Body() task:Task,): Promise<Task> {
+        return this.tasksService.update(Number(id), task);
+    }
+
+    @Delete(':id')
+    deleteTask(@Param('id') id: string) {
+        return this.tasksService.delete(Number(id));
+    }
 }
--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { Task } from 'src/entities/task.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, InsertResult, Repository } from 'typeorm';
 
 @Injectable()
 export class TasksService {
@@ -11,7 +11,34 @@ export class TasksService {
     ) {}
 
 
-    getTasks(): Promise<Task[]>{
+    async getTasks(): Promise<Task[]>{
         return this.tasksRepository.find();
     }
+
+    async addTask(task: Task): Promise<InsertResult> {
+        return this.tasksRepository.insert(task);
+    }
+
+    async findOne(id: number): Promise<Task> {
+        return this.tasksRepository.findOne(id);
+    }
+
+    async update(id: number, task:Task): Promise<Task> {
+        const taskToUpdate = await this.findOne(id);
+        if(taskToUpdate === undefined) {
+            throw new NotFoundException();
+        }
+        await this.tasksRepository.update(id, task);
+        return this.findOne(id);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+        const taskToDelete = await this.findOne(id);
+        if(taskToDelete === undefined) {
+            throw new NotFoundException();
+        }
+        
+        return this.tasksRepository.delete(id);
+    }
+
 }
