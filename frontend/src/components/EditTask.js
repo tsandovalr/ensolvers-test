ee1eaa2ee226581b555e8bb55c6b98d722c7717f
tsandovalr <tsandovalromay@gmail.com>
2022-01-09 18:00:17 -0400

    Patch method updated

--- frontend/src/components/EditTask.js
+++ frontend/src/components/EditTask.js
@@ -3,44 +3,49 @@ import React, { Fragment, useState } from "react";
 const EditTask = ({ task }) => {
   const [title, setTitle] = useState(task.title);
 
-  const updateTitle = async (e) => {
+  const updateTitle = async e => {
     e.preventDefault();
     try {
       const body = { title };
       const response = await fetch(
         `https://ensolvers-back.herokuapp.com/api/tasks/${task.id}`,
         {
-          method: "PUT",
+          method: "PATCH",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(body),
         }
       );
       console.log(response);
       window.location = "/";
+      
     } catch (err) {
       console.error(err.message);
     }
   };
-
+  
   return (
     <Fragment>
       <button
         type="button"
         className="btn bg-light border-primary text-primary "
-        data-toggle="modal"
-        data-target={`#id${task.id}`}
+        data-bs-toggle="modal"
+        data-bs-target={`#id${task.id}`}
       >
         Edit
       </button>
       <div
-        className="modal"
+        className="modal fade"
         id={`id${task.id}`}
+        role="dialog"
+        aria-hidden="true"
+        aria-labelledby="title-label"
         onClick={() => setTitle(task.title)}
       >
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h4 className="modal-title">Edit Task</h4>
+              <h4 className="modal-title"
+              id="title-label">Edit Task</h4>
               <button
                 type="button"
                 className="close"
@@ -55,7 +60,7 @@ const EditTask = ({ task }) => {
                 type="text"
                 className="form-control"
                 value={title}
-                onChange={(e) => setTitle(e.target.value)}
+                onChange={e => setTitle(e.target.value)}
               />
             </div>
             <div className="modal-footer">
@@ -63,7 +68,7 @@ const EditTask = ({ task }) => {
                 type="button"
                 className="btn bg-light border-primary text-primary "
                 data-dismiss="modal"
-                onClick={(e) => updateTitle(e)}
+                onClick={e => updateTitle(e)}
               >
                 Edit
               </button>
@@ -71,7 +76,7 @@ const EditTask = ({ task }) => {
                 type="button"
                 className="btn bg-light border-danger text-danger"
                 data-dismiss="modal"
-                onClick={() => setTitle(task.title)}
+                onClick={() => {window.location = "/";}}
               >
                 Close
               </button>
